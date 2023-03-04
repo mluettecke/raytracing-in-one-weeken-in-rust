3ce2a67bece0c286330e22afb062b2c159fcf61b
Marcel Lüttecke <mluettecke@posteo.net>
2023-03-04 16:05:01 +0100

    ✨ Add code for chapter 2.2 "Creating an Image File"

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 fn main() {
-    const IMAGE_WIDTH: u64 = 256;
-    const IMAGE_HEIGHT: u64 = 256;
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
 
-    println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("255");
+    println!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
         for i in 0..IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
-            let b = 0.25;
+            let r: f64 = (i as f64) / (IMAGE_WIDTH - 1) as f64;
+            let g: f64 = (j as f64) / (IMAGE_HEIGHT - 1) as f64;
+            let b: f64 = 0.25;
 
-            let ir = (255.999 * r) as u64;
-            let ig = (255.999 * g) as u64;
-            let ib = (255.999 * b) as u64;
+            let ir: u64 = (255.999 * r) as u64;
+            let ig: u64 = (255.999 * g) as u64;
+            let ib: u64 = (255.999 * b) as u64;
 
-            println!("{} {} {}", ir, ig, ib);
+            println!("{} {} {}", ir, ig, ib)
         }
     }
 }
